b343c4a307f4d91123a426ea59f3291b8d522cbc
Abhay-Sandhu <abhay.sandhu07@gmail.com>
2024-07-19 18:35:20 +0530

    tetris refactor

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -3,21 +3,6 @@ from random import choice
 from pygame.sprite import Group
 from pygame import display, image, transform, draw, sprite, key, Color, K_LEFT, K_RIGHT, K_DOWN, time 
 
-#Constants and variables
-GREEN = Color(0, 255, 0, 255)
-WIDTH = 400
-HEIGHT = 560
-BLOCK_SIZE = 20
-PANEL_SIZE = 160
-ROWS = ((HEIGHT- PANEL_SIZE)// 20) 
-COLS = (WIDTH)//20
-drop_zone_x = (WIDTH - BLOCK_SIZE * 4)//2
-drop_zone_y = 0
-GRID_COLOR = Color(31, 25, 132)
-PANEL_COLOR = Color(25, 20, 122)
-screen = display.set_mode((WIDTH, HEIGHT))
-
-
 class Block(sprite.Sprite):
     COLORS = iter([1,2,3,4])
     COLOR_BLOCK_DICT = {
@@ -26,13 +11,13 @@ class Block(sprite.Sprite):
             3: image.load('Tetris/assets/3.png'),
             4: image.load('Tetris/assets/4.png')
             }
-    def __init__(self, row, col,):
+    def __init__(self, game, row, col,):
         super().__init__()
         self.color = next(Block.COLORS)
-        self.image = transform.scale(image.load('Tetris/assets/block.png'), (BLOCK_SIZE, BLOCK_SIZE))
+        self.image = transform.scale(image.load('Tetris/assets/block.png'), (game.block_size, game.block_size))
         self.rect = self.image.get_rect()
-        self.rect.x = col * BLOCK_SIZE
-        self.rect.y = row * BLOCK_SIZE
+        self.rect.x = col * game.block_size
+        self.rect.y = row * game.block_size
 
 # Shape Class
 class Shape():
@@ -53,52 +38,14 @@ class Shape():
         self.images = []
         self.rects = []
     def draw(self):
-        x = drop_zone_x
-        y = drop_zone_y
-        for cell in self.shape:
-            img = transform.scale(self.block, (BLOCK_SIZE, BLOCK_SIZE))
-            img_rect = img.get_rect()
-            img_rect.x = x + (cell % 4) * BLOCK_SIZE
-            img_rect.y = y + (cell // 4) * BLOCK_SIZE
-            self.images.append(img)
-            self.rects.append(img_rect)
-            screen.blit(img, (img_rect.x, img_rect.y)) 
-    def drop(self):
-        global drop_zone_y
-        x = drop_zone_x
-        y = drop_zone_y
-        drop_zone_y += BLOCK_SIZE
-        for cell in self.shape:
-            img = transform.scale(self.block, (BLOCK_SIZE, BLOCK_SIZE))
-            img_rect = img.get_rect()
-            img_rect.x = x + (cell % 4) * BLOCK_SIZE
-            img_rect.y = y + (cell // 4) * BLOCK_SIZE
-            self.images.append(img)
-            self.rects.append(img_rect)
-            screen.blit(img, (img_rect.x, img_rect.y)) 
-        time.delay(1000)
-
-
+        pass
 
 # Game Class
-class Game():
+class Master():
     def __init__(self):
-        # Create gameboard
-        self.grid = [[0 for x in range(COLS)] for y in range(ROWS)]
-        self.score = 0
         self.next = None
-        self.end = False
-        
-        
-    # Create a grid
-    def create_grid(self):
-        for x in range(COLS):
-            draw.line(screen, GRID_COLOR, (x * BLOCK_SIZE, 0), (x * BLOCK_SIZE, HEIGHT - PANEL_SIZE))
-        for y in range(ROWS):
-            draw.line(screen, GRID_COLOR, (0, y * BLOCK_SIZE), (WIDTH, y * BLOCK_SIZE))
-        
-    def create_panel(self):
-        draw.rect(screen, PANEL_COLOR,(0, HEIGHT - PANEL_SIZE, WIDTH, PANEL_SIZE))
+
+    
     def generate_shape(self):
         if self.next is None:
             self.next = Shape()
@@ -107,9 +54,3 @@ class Game():
         #color,block,type,shape
 
 
-    
-    # create bottom panel
-    # show upcoming shape
-    # generate new shapes
-    # save shape to gameboard
-    # remove row
--- Tetris/game.py
+++ Tetris/game.py
@@ -1,61 +1,100 @@
-import pygame
-from pygame import draw
 from random import randint
-import Tetris
-
-pygame.init()
-pygame.font.init()
-pygame.mixer.init()
-
-RED = pygame.Color(255, 0, 0, 255)     # Fully opaque red
-GREEN = pygame.Color(0, 255, 0, 255)   # Fully opaque green
-BLUE = pygame.Color(0, 0, 255, 255)    # Fully opaque blue
-WHITE = pygame.Color(255, 255, 255, 255) # Fully opaque white
-BG_COLOR = pygame.Color(31, 25, 76, 255)
-GRID_COLOR = Tetris.GRID_COLOR
-
-SCORE = 0
-FPS = 60
-VOLUME = 0.03
-BLOCK_SIZE = Tetris.BLOCK_SIZE
-WIDTH = Tetris.WIDTH
-HEIGHT = Tetris.HEIGHT
-
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('TETRIS!!!')
-clock = pygame.time.Clock()
-
-font = pygame.font.Font(None, 40)
-font.set_bold(True)
-win_text = font.render('YOU WIN!', 1, GREEN)
-lose_text = font.render('YOU LOSE!', 1, RED)
-
-def create_drop_zone(X,Y):
-        for x in range(X, X + BLOCK_SIZE * 5, BLOCK_SIZE):
-            draw.line(screen, RED, (x, 0), (x, Y + BLOCK_SIZE * 4))
-        for y in range(Y, Y + BLOCK_SIZE * 5, BLOCK_SIZE):
-            draw.line(screen, RED, (X, y), (X + BLOCK_SIZE * 4, y))
+import pygame
+from Tetris import Shape, Block, Master
 
 
-game = Tetris.Game()
-shape = Tetris.Shape()
-end = game.end
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    if end != True:
-        screen.fill(BG_COLOR)
-        game.create_grid()
-        game.create_panel()
-        shape.drop()
-        create_drop_zone(Tetris.drop_zone_x, Tetris.drop_zone_y)
-    else:
-        end = False
+class Game():
+    RED = pygame.Color(255, 0, 0, 255)     # Fully opaque red
+    GREEN = pygame.Color(0, 255, 0, 255)   # Fully opaque green
+    BLUE = pygame.Color(0, 0, 255, 255)    # Fully opaque blue
+    WHITE = pygame.Color(255, 255, 255, 255) # Fully opaque white
+    BG_COLOR = pygame.Color(31, 25, 76, 255)
+    GRID_COLOR = pygame.Color(31, 25, 132, 255)
+    PANEL_COLOR = pygame.Color(25,25,25,255)
+    SCORE = 0
+    FPS = 60
+    VOLUME = 0.03
+    BLOCK_SIZE = 20
+    WIDTH = 400
+    HEIGHT = 560
+    PANEL_SIZE = 160
+    ROWS = (HEIGHT - PANEL_SIZE) // BLOCK_SIZE
+    COLS = (WIDTH) // BLOCK_SIZE
+    
+    def __init__(self):
+        pygame.init()
+        pygame.font.init()
+        pygame.mixer.init()
 
-    pygame.display.update()
-    clock.tick(FPS)
+        self.block_size = Game.BLOCK_SIZE
+        self.width = Game.WIDTH
+        self.height = Game.HEIGHT
+        self.panel_size = Game.PANEL_SIZE
+        self.rows = Game.ROWS
+        self.cols = Game.COLS
+        self.score = 0
+        
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Tetris!')
+        self.clock = pygame.time.Clock()
+        
+        #self.food_sound = pygame.mixer.Sound('Snake/assets/food.mp3')
+        #self.move_sound = pygame.mixer.Sound('Snake/assets/move.mp3')
+        #self.game_over_sound = pygame.mixer.Sound('Snake/assets/gameover.mp3')
 
-pygame.quit()
-exit()
+        #self.game_over_sound.set_volume(Game.VOLUME)
+        #self.food_sound.set_volume(Game.VOLUME)
+        #self.move_sound.set_volume(Game.VOLUME)
+
+        self.font = pygame.font.Font(None, 40)
+        self.font.set_bold(True)
+        self.win_label = self.font.render('YOU WIN!', 1, Game.GREEN)
+        self.lose_label = self.font.render('YOU LOSE!', 1, Game.RED)
+        #self.play_bgm()
+        self.new_game()
+
+    def new_game(self):
+        self.score = 0
+    
+   # def play_bgm(self):
+       # pygame.mixer.music.load('Snake/assets/music.mp3')
+       # pygame.mixer.music.set_volume(Game.VOLUME * 0.3)
+       # pygame.mixer.music.play()
+    def draw_grid(self):
+        [pygame.draw.line(self.screen, Game.GRID_COLOR, (x * self.block_size, 0), (x * self.block_size, self.height - self.panel_size)) for x in range(0, self.cols)]
+        [pygame.draw.line(self.screen, Game.GRID_COLOR, (0, y * self.block_size), (self.width, y * self.block_size)) for y in range(0, self.rows + 1)]
+    
+    def draw_panel(self):
+        pygame.draw.rect(self.screen, Game.PANEL_COLOR,(0, self.height - self.panel_size, self.width, self.panel_size))
+
+    def update(self):
+        pygame.display.update()
+        self.clock.tick(Game.FPS)
+
+    def draw(self):
+        self.screen.fill(Game.BG_COLOR)
+        self.draw_grid()
+        self.draw_panel()
+        score_text = self.font.render(f"SCORE: {self.score}", 1, Game.WHITE)
+        self.screen.blit(score_text, (self.width - score_text.get_width() - 50, self.height - self.panel_size/2))
+
+    
+    def event_check(self, run):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                return run
+        return run
+    
+    def run(self):
+        run = True
+        while run:
+            run = self.event_check(run)
+            self.draw()
+            self.update()
+        pygame.quit()
+        exit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
